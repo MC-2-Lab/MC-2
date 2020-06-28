5d8e9dc2dfa4972ee592e56a65623194eaa2bd47
ywz978020607 <ywzsunny2012@163.com>
2020-06-28 17:08:32 +0800

    update

---
+++ 一次性存入过程 记录/新增add.py
@@ -0,0 +1,11 @@
+from config import *
+
+add_context=\
+    """<li><strong>[2020-04-15]</strong>
+Dr. Xin Deng, our previous master student has obtained her PhD degree from Imperial College, and joined our group as a research associate professor. Welcome back, Dr Deng.
+</li>"""
+
+
+add_ini('data.ini',add_context)
+print("ok")
+
