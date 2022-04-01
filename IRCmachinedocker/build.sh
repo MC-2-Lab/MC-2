b77b69dd942994a462480916332468aabd4323b0
ywz978020607 <ywzsunny@buaa.edu.cn>
2022-04-01 15:56:59 +0800

    up

---
+++ IRCmachinedocker/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+apt-get clean
+
+apt-get update --fix-missing
+apt-get install -y python3 python3-pip nginx 
+# systemctl enable nginx.service
+
+pip3 install psutil
+pip3 install -r /tmp/requirements.txt
+
+
+# export PATH = xx
