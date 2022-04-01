3f89730723ece696f8ab2f990116950b26c9da7a
ywz978020607 <ywzsunny@buaa.edu.cn>
2022-04-01 23:16:29 +0800

    up

--- machine/IRCmachinedocker/build.sh
+++ machine/IRCmachinedocker/build.sh
@@ -3,7 +3,7 @@ apt-get clean
 
 apt-get update --fix-missing
 apt-get install -y python3 python3-pip nginx ssh sshpass
-# systemctl enable nginx.service
+ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 
 pip3 install psutil
 pip3 install -r /tmp/requirements.txt
