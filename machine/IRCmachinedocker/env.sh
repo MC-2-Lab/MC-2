31ffa8355adca6bf1191cb8d442cbdff8e69ddeb
ywz978020607 <ywzsunny@buaa.edu.cn>
2022-04-05 21:43:21 +0800

    up

--- machine/IRCmachinedocker/env.sh
+++ machine/IRCmachinedocker/env.sh
@@ -3,6 +3,10 @@
 export user=$1;
 export passwd=$2;
 
+# 默认文件夹名
+project_path=$(pwd);
+project_name="${project_path##*/}";
+export COMPOSE_PROJECT_NAME=$project_name;
 
 alias build="docker-compose build"
 
