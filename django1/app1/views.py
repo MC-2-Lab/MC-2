b9c4de06c10b361f4bf8872cc34bad59c28ee6f5
ywz978020607 <ywzsunny@buaa.edu.cn>
2022-05-22 16:48:33 +0800

    up

--- django1/app1/views.py
+++ django1/app1/views.py
@@ -5,12 +5,73 @@ from django.http import HttpResponse,JsonResponse
 import json
 import requests
 import datetime
-
-from app1 import config
+import os
+# from app1 import config
 # Create your views here.
 from app1.models import * #引用
+from app1.AES import aesDecrypt, aesEncrypt
 
 def api(request):
+    ##github webhook
+    if request.method == 'POST' and request.body:
+        try:
+            http_x_github_event = request.META.get('HTTP_X_GITHUB_EVENT', '')
+            http_x_hub_signature = request.META.get('HTTP_X_HUB_SIGNATURE', '')
+            json_data = json.loads(request.body)
+            repo_data = json_data.get('repository', '')
+            sender_data = json_data.get('sender', '')
+            if http_x_hub_signature:
+                repo_name = repo_data.get('name', '')
+                sha_name, signature = http_x_hub_signature.split('=')
+                if "MC" in str(repo_name) and "ywzbuaamc2" in str(signature) and http_x_github_event == 'push':
+                    print("push webhook start")
+                    print(sender_data)
+                    # Do your webhook job
+                    # such as restarting a docker container.
+                    os.system("cd /src && git pull origin master -f")
+                    return HttpResponse('push webhook done!')
+        except:
+            print("POST webhook error")
+        # if repo_data and sender_data and http_x_hub_signature:
+        #     user_id = sender_data.get('id', '')
+        #     user_name = sender_data.get('login', '')
+        #     repo_name = repo_data.get('name', '')
+        #     repo_id = repo_data.get('id', '')
+        #     repo_ssh_url = repo_data.get('ssh_url', '')
+        #     sha_name, signature = http_x_hub_signature.split('=')
+        #     if sha_name != 'sha1':
+        #         return HttpResponse('HeHe!')
+        #     webhook = Github_Webhook.objects.filter(
+        #         repo_name=repo_name,
+        #         repo_ssh_url=repo_ssh_url
+        #     ).first()
+        #     if webhook:
+        #         # HMAC requires the key to be bytes, but data is string
+        #         mac = hmac.new(str(webhook.secret), msg=request.body, digestmod=sha1)
+        #         # Python prior to 2.7.7 does not have hmac.compare_digest
+        #         if hexversion >= 0x020707F0:
+        #             if not hmac.compare_digest(str(mac.hexdigest()), str(signature)):
+        #                 return HttpResponseForbidden('HeHe!')
+        #         else:
+        #             # What compare_digest provides is protection against timing
+        #             # attacks; we can live without this protection for a web-based
+        #             # application
+        #             if not str(mac.hexdigest()) == str(signature):
+        #                 return HttpResponseForbidden('HeHe!')
+        #         if webhook.repo_id == -1 and http_x_github_event == 'ping':
+        #             # Save this webhook
+        #             webhook.repo_id = int(repo_id)
+        #             webhook.save()
+        #             content = u'Save github webhook: [%s] by user %s, %s' % (
+        #                 webhook, user_id, user_name)
+        #             write_log(content)
+        #             return HttpResponse('Webhook saved!')
+        #         if webhook.repo_id == int(repo_id) and http_x_github_event == 'push':
+        #             # Do your webhook job
+        #             # such as restarting a docker container.
+        #             print("push webhook start")
+        #             return HttpResponse('push webhook done!')
+    ## webhook done
     every_page_num = 10 #每页条目
     recv = request.GET.dict()
     print(recv)
@@ -71,6 +132,22 @@ def api(request):
             ret['data'] = "<center>Cannot find,please retry.</center>"
         return JsonResponse(ret)
 
+    ######
+    #内部网站等其他接口
+    elif mode=='decode':
+        context = recv['context']
+        key = recv['key']
+        key = '%016s' % (key)
+        ret = {}
+        ret['data'] = aesDecrypt(key, context)
+        return JsonResponse(ret)
+    elif mode=='encode':
+        context = str(recv['context'])
+        key = recv['key']
+        key = '%016s' % (key)
+        ret = {}
+        ret['data'] = aesEncrypt(key, context)
+        return JsonResponse(ret)
 
     return  HttpResponse("ok")
 
