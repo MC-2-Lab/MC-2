3002f738d6b93aed1fa06332d78f0753cf076820
ywz978020607 <ywzsunny@buaa.edu.cn>
2022-05-28 12:46:57 +0800

    up

--- dev_start.sh
+++ dev_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo ${gitlabip}
-sshpass -p ${passwd} ssh -L 8081:${gitlabip}:8081 ywz@${openip} -p ${openport} &
-sshpass -p ${passwd} ssh -L 8022:${gitlabip}:8022 ywz@${openip} -p ${openport} &
+sshpass -p ${passwd} ssh -L -o StrictHostKeyChecking=no 8081:${gitlabip}:8081 ywz@${openip} -p ${openport} &
+sshpass -p ${passwd} ssh -L -o StrictHostKeyChecking=no 8022:${gitlabip}:8022 ywz@${openip} -p ${openport} &
 
 python django1/manage.py runserver 0.0.0.0:8000 > docker/log/django.log 2>&1
