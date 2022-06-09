5008dbba9f1284cc64b306f914b001e87e7528c2
ywz978020607 <ywzsunny@buaa.edu.cn>
2022-06-09 14:35:37 +0800

    up

--- django1/app1/views.py
+++ django1/app1/views.py
@@ -32,7 +32,7 @@ def api(request):
                     # Do your webhook job
                     # such as restarting a docker container.
                     # os.system("cd /src && git fetch --all && git reset --hard origin/master && git pull origin master -f && chmod +x dev_start.sh && docker restart djangoIRC")
-                    os.system("cd /src && git pull origin master -f && chmod +x dev_start.sh && docker restart djangoIRC")
+                    os.system("cd /src && git add -A && git commit -m 'save' && git pull origin master -f && chmod +x dev_start.sh && docker restart djangoIRC")
                     # refresh DNS
                     os.system("cd /src/machine/IRCmachinedocker && python3 code/aliyun.py")
                     return HttpResponse('push webhook done!')
