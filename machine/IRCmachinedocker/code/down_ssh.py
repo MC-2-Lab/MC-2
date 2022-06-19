70169426711ad6c7b3bd3e45013981d6ec638ac7
ywz978020607 <ywzsunny@buaa.edu.cn>
2022-06-19 21:38:15 +0800

    up

--- machine/IRCmachinedocker/code/down_ssh.py
+++ machine/IRCmachinedocker/code/down_ssh.py
@@ -1,18 +1,32 @@
 # run this in .ssh/ and add `Include config.d/*` in .ssh/config first line.
 # PS.jump_sever config need to add manually in .ssh/config, which is not keep in online file.
 # If you need other user temporarily, you can input: ssh youruser@Host, like: ssh liutie@30901
+from imageio import save
 import requests
 import sys, os
+import platform
 
 youruser = "ywz"
 
 if not os.path.exists("config.d"):
     os.mkdir("config.d")
 file = "http://buaamc2.net/machine/IRCmachinedocker/code/config"
-save_as = "config.d/BUAAMC2"
-os.remove(save_as)
+save_as = "config.d/config"
+if os.path.exists(save_as):
+    os.remove(save_as)
+
+if platform.system() == "Windows":
+    os.system("copy {} {}".format("config", save_as.replace("/","\\")))
+else:
+    os.system("cp -p {} {}".format("config", save_as))
+
 r = requests.get(file)
-with open(save_as, 'wb') as f:
+with open("down", 'wb') as f:
     # f.write(r.content)
     f.write(r.content.decode().replace("ywz", youruser).encode()) #替换公用默认账号
     f.close()
+
+if not platform.system() == "Windows":
+    os.system("cat {} > {}".format("down", save_as.replace("/","\\")))
+else:
+    print("手动复制down内容到config.d/配置文件中，解决权限冲突")
