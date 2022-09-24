02ffe9dc6f767fb2a17c5d917ef60fce1a2f2e39
邓宇凡(Yufan Deng) <59913015+eecoder-dyf@users.noreply.github.com>
2022-09-24 20:17:19 +0800

    change source to ustc

--- machine/IRCmachinedocker/build.sh
+++ machine/IRCmachinedocker/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 apt-get clean
-
+sed -i 's/deb.debian.org/mirrors.ustc.edu.cn/g' /etc/apt/sources.list
 apt-get update --fix-missing
 apt-get install -y python3 python3-pip nginx ssh sshpass
 ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
