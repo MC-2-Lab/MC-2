d6a6ff95bce19c60f78084b8e637356ca52a6d11
dyf <yufan.deng@foxmail.com>
2025-11-09 15:02:27 +0800

    update on 30901

--- machine/DockerOfBUAALogin/docker/build.sh
+++ machine/DockerOfBUAALogin/docker/build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # #换源
-# sed -i s@/archive.ubuntu.com/@/mirrors.aliyun.com/@g /etc/apt/sources.list
+sed -i 's/deb.debian.org/mirrors.ustc.edu.cn/g' /etc/apt/sources.list.d/debian.sources
 # sed -i s@/security.ubuntu.com/@/mirrors.aliyun.com/@g /etc/apt/sources.list
 # apt clean
 apt-get update -y
-apt-get install -y sudo ssh git net-tools sshpass
+apt-get install -y sudo net-tools
 apt-get install -y python3-pip
 
-pip3 install -r /tmp/requirements.txt
+pip3 install -r /tmp/requirements.txt -i https://mirrors.ustc.edu.cn/pypi/web/simple
--- machine/sshrdocker/docker/env.sh
+++ machine/sshrdocker/docker/env.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
 export passwd=$1;
-export user=$2
-export pwd=$3
 
 alias build="docker-compose build --build-arg passwd=$passwd"
 # TODO: 使用脚本，包括初次使用，退出-继续运行/停止，重入等
